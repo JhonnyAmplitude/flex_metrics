e39fd410f95e927da07cb1e9f7cd4b8795d707c2
JhonnyAmplitude <jhonnyamplituda@gmail.com>
2025-03-12 13:56:47 +0500

    feat: docker-compose file

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from fastapi import FastAPI
 import requests
 from fastapi.responses import StreamingResponse
 from io import StringIO
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
-API_URL = 'https://api-metrika.yandex.ru/stat/v1/data.csv'
-API_token = 'y0__xDOv9UeGOf0NSCdltHAEuH0WBTuKpjd6EcSRsWjGXqaC-3J'
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+API_token = os.getenv("API_TOKEN")
 params = {
     'date1': '6daysAgo',
     'date2': 'today',
     'id': 64417021,
-    'metrics': 'ym:s:visits,ym:s:users',
+    "metrics": [
+        "ym:s:visits",
+        "ym:s:users",
+        "ym:s:bounceRate",
+        "ym:s:pageDepth",
+        "ym:s:avgVisitDurationSeconds"
+    ],
     'dimensions': 'ym:s:TrafficSource',
     'limit': 100
 }
@@ -21,8 +31,13 @@ async def get_metrika_data():
     response = requests.get(API_URL, params=params, headers={'Authorization': f'OAuth {API_token}'})
 
     if response.status_code == 200:
-        # Преобразуем полученные данные в поток для ответа
-        data = StringIO(response.text)
-        return StreamingResponse(data, media_type="text/csv")
+        # Указываем разделители для разных региональных форматов в Excel
+        csv_data = response.text.replace(',', ';')
+        # Добавляем BOM для корректного отображения UTF-8
+        data = StringIO('\ufeff' + csv_data)
+        headers = {
+            "Content-Disposition": "attachment; filename=metrics.csv"
+        }
+        return StreamingResponse(data, media_type="text/csv", headers=headers)
     else:
-        return {"error": "Failed to retrieve data", "status_code": response.status_code}
+        return {"error": "Ошибка получения данных", "status_code": response.status_code}
